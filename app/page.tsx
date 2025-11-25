91e48c454feb043d6dbd7c0f3ef3ae892dff27b3
Dip Chakraborty <dip.preneurlab@gmail.com>
2025-11-25 17:48:40 +0600

    last before docker

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 'use client';
-'use client';
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 
 import AppClient from './AppClient';
 
